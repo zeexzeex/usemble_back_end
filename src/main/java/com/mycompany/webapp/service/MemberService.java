a8813fbba51e96df0fd0cb214e8e3a209076803a
김명환 <myeonghwan57@gmail.com>
2024-06-25 13:40:36 +0900

    feat: Add member service 작성

--- src/main/java/com/mycompany/webapp/service/MemberService.java
+++ src/main/java/com/mycompany/webapp/service/MemberService.java
@@ -1,8 +1,19 @@
 package com.mycompany.webapp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.mycompany.webapp.dao.MemberDao;
+import com.mycompany.webapp.dto.Member;
 
 @Service
 public class MemberService {
+
+	@Autowired
+	private MemberDao memberDao;
+
+	public void join(Member member) {
+		memberDao.insert(member);
+	}
 
 }
